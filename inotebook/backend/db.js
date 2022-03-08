0a5f1e01afcf1ec84969bfbc6daf2c6c44dc4f5c
Bhupender Soni <official.soni532@gmail.com>
2022-03-08 16:57:37 +0530

    using MERN stack in iNoteBook app

---
+++ inotebook/backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
+const connectTOMongo = () => {
+    mongoose.connect(mongoURI, ()=>{
+        console.log('Connected to Mongo Successfully')
+    })
+}
+module.exports = connectTOMongo;
---
+++ inotebook/backend/models/Notes.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const NotesSchema = new Schema({
+    title:{
+        type: String, 
+        required: true, 
+        unique: true
+    }, 
+    description:{
+        type: String, 
+        required: true,
+    }, 
+    tag:{
+        type: String, 
+        default: "General"
+    },
+    date:{
+        type: String, 
+        required: true
+    }
+})
+
+module.exports = mongoose.model('notes', NotesSchema)
